0a33887e84399c9ea51ee3ee13ee264c5f8bbc53
niikhill <5293647+niikhill@users.noreply.github.com>
2019-01-06 15:00:58 +0000

    Merge tag 'android-9.0.0_r21' into pie

--- core/build_id.mk
+++ core/build_id.mk
@@ -17,5 +17,4 @@
 # (like "MAIN") or a branch name and a release candidate
 # (like "CRB01").  It must be a single word, and is
 # capitalized by convention.
-
-export BUILD_ID=PPR2.181005.003
+export BUILD_ID=PQ1A.181205.006
